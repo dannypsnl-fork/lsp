e419145a1351a1490a428c76b731afcce8c309d9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-19 17:14:23 +0800

    lsp(update)

--- methods.rkt
+++ methods.rkt
@@ -7,9 +7,9 @@
          "responses.rkt")
 
 ;; TextDocumentSynKind enumeration
-(define TextDocSync-None 0)
-(define TextDocSync-Full 1)
-(define TextDocSync-Incremental 2)
+(define TextDocSync.None 0)
+(define TextDocSync.Full 1)
+(define TextDocSync.Incremental 2)
 
 (define already-initialized? #f)
 
@@ -19,29 +19,22 @@
                  ['capabilities (? jsexpr? capabilities)])
      (define sync-options
        (hasheq 'openClose #t
-               'change TextDocSync-Incremental
+               'change TextDocSync.Full
                'willSave #f
                'willSaveWaitUntil #f))
-     (define renameProvider
-       (match capabilities
-         [(hash-table ['textDocument 
-                       (hash-table ['rename
-                                    (hash-table ['prepareSupport #t])])])
-          (hasheq 'prepareProvider #t)]
-         [_ #t]))
      (define server-capabilities
        (hasheq 'textDocumentSync sync-options
-               'hoverProvider #t
-               'definitionProvider #t
-               'referencesProvider #t
-               'completionProvider (hasheq 'triggerCharacters (list "("))
-               'signatureHelpProvider (hasheq 'triggerCharacters (list " " ")" "]"))
-               'renameProvider renameProvider
+               'hoverProvider #f
+               'definitionProvider #f
+               'referencesProvider #f
+               'completionProvider #f
+               'signatureHelpProvider #f
+               'renameProvider #f
                'documentHighlightProvider #t
-               'documentSymbolProvider #t
-               'documentFormattingProvider #t
-               'documentRangeFormattingProvider #t
-               'documentOnTypeFormattingProvider (hasheq 'firstTriggerCharacter ")" 'moreTriggerCharacter (list "\n" "]"))))
+               'documentSymbolProvider #f
+               'documentFormattingProvider #f
+               'documentRangeFormattingProvider #f
+               'documentOnTypeFormattingProvider #f))
 
      (set! already-initialized? #t)
      (success-response id (hasheq 'capabilities server-capabilities))]
